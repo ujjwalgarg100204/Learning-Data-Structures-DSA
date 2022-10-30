f60949e825a75b691da8b4db918198fa6af0caba
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2022-10-30 18:55:33 +0530

    implement non circular queue singly linked list implementation
    Signed-off-by: Ujjwal Garg <ujjwalgarg100204@gmail.com>

---
+++ data_structures/queue/non-circular/ll_implementation.c
@@ -0,0 +1,77 @@
+#include "ll_implementation.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+Queue_List_S *newQueue_List_S() {
+    Queue_List_S *queue = malloc(sizeof(Queue_List_S));
+    queue->front = NULL;
+    queue->rear = NULL;
+    return queue;
+}
+
+void freeQueue_List_s(Queue_List_S *queue) {
+    clear_Queue_List_S(queue);
+    free(queue);
+}
+
+bool isEmpty_Queue_List_S(Queue_List_S *queue) {
+    return queue->front == NULL && queue->rear == NULL;
+}
+
+void enqueue_Queue_List_S(Queue_List_S *queue, long long int element) {
+    Node_S *new = newNode_S(element);
+
+    // if first enqueue operation
+    if (isEmpty_Queue_List_S(queue)) {
+        queue->front = new;
+        queue->rear = new;
+        return;
+    }
+
+    queue->rear->next = new;
+    queue->rear = new;
+}
+
+ll dequeue_Queue_List_S(Queue_List_S *queue) {
+    // check for empty queue
+    if (isEmpty_Queue_List_S(queue)) return -1;
+
+    ll toReturn = queue->front->data;
+
+    Node_S *next = queue->front->next;
+    free(queue->front);
+    queue->front = next;
+
+    // if last dequeue operation
+    if (queue->front == NULL) queue->rear = NULL;
+
+    return toReturn;
+}
+
+ll getRear_Queue_List_S(Queue_List_S *queue) {
+    return isEmpty_Queue_List_S(queue) ? -1 : queue->rear->data;
+}
+
+ll getFront_Queue_List_S(Queue_List_S *queue) {
+    return isEmpty_Queue_List_S(queue) ? -1 : queue->front->data;
+}
+
+void clear_Queue_List_S(Queue_List_S *queue) {
+    while (queue->front != NULL) {
+        Node_S *next = queue->front->next;
+        freeNode_S(queue->front);
+        queue->front = next;
+    }
+}
+
+void printQueue_List_S(Queue_List_S *queue) {
+    if (isEmpty_Queue_List_S(queue)) {
+        printf("empty queue");
+        return;
+    }
+
+    for (Node_S *curr = queue->front; curr != NULL; curr = curr->next)
+        printf("%lld ", curr->data);
+}
+
+
---
+++ data_structures/queue/non-circular/ll_implementation.h
@@ -0,0 +1,31 @@
+#ifndef CODES_LL_IMPLEMENTATION_H
+#define CODES_LL_IMPLEMENTATION_H
+
+#include <stdbool.h>
+#include "../../linked_list/singly_node.h"
+
+typedef struct Queue_List_S {
+    Node_S *front;
+    Node_S *rear;
+} Queue_List_S;
+
+
+Queue_List_S *newQueue_List_S();
+
+void freeQueue_List_s(Queue_List_S *queue);
+
+bool isEmpty_Queue_List_S(Queue_List_S *queue);
+
+void enqueue_Queue_List_S(Queue_List_S *queue, ll element);
+
+ll dequeue_Queue_List_S(Queue_List_S *queue);
+
+ll getRear_Queue_List_S(Queue_List_S *queue);
+
+ll getFront_Queue_List_S(Queue_List_S *queue);
+
+void clear_Queue_List_S(Queue_List_S *queue);
+
+void printQueue_List_S(Queue_List_S *queue);
+
+#endif //CODES_LL_IMPLEMENTATION_H
